45a2db8b921f99794192b9b9ed9593536b3e9914
Fatwa Maulana <babangkece0873@gmail.com>
2026-07-08 10:40:22 +0700

    Disable middleware debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,3 @@
-export { default } from "next-auth/middleware";
-
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/sekolah/:path*",
-    "/vendor/:path*",
-    "/menu/:path*",
-    "/distribusi/:path*",
-    "/riwayat/:path*",
-    "/search/:path*",
-    "/api/sekolah/:path*",
-    "/api/vendor/:path*",
-    "/api/menu/:path*",
-    "/api/distribusi/:path*",
-    "/api/dashboard/:path*",
-    "/api/search/:path*",
-  ],
+  matcher: [],
 };
